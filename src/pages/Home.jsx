6bf36984b4790da1c481173dcd235584ae5be169
rohan dangi <rohan5250ssbj@gmail.com>
2026-03-07 17:55:27 +0530

    fixed responsiveness

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import Hero from '../Component/Hero';
 import PartnershipOpportunities from '../Component/PartnershipOpportunities';
 import CTA from '../Component/CTA';
-import TestimonialV2 from '../components/ui/testimonial-v2';
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <PartnershipOpportunities />
-      <TestimonialV2 />
       <CTA />
     </div>
   );
